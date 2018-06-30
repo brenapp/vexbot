9b331038bd79f2617b5a592cab4400f0bacdd907
MayorMonty <artificalchamber@gmail.com>
2018-06-29 22:39:45 -0400

    Log channel name

--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -1,21 +1,23 @@
 import { addMessageHandler } from "./message";
+import { TextChannel } from "discord.js";
 
 // Dismiss messages from a bot, we don't take their kind around here!
 addMessageHandler(message => message.author.bot);
 
 // Message Logging
 addMessageHandler(message => {
+  let channel = message.channel as TextChannel;
   console.log(
-    `Message from ${message.author.username}#${message.author.discriminator} (${
-      message.author.id
-    }): ${message.content}`
+    `${message.author.username}#${message.author.discriminator} in #${
+      channel.name
+    }: ${message.content}`
   );
   return false;
 });
 
 // Ping!
 addMessageHandler(message => {
-  if (message.content.toLowerCase() == "ping") {
+  if (message.content.toLowerCase() == "!ping") {
     message.reply("Pong!");
     return true;
   } else {
