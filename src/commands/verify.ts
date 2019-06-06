359564077e5480e32753260432206a463e18f272
MayorMonty <artificalchamber@gmail.com>
2019-06-06 01:57:06 -0400

    Verify command

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -1,7 +1,18 @@
-import { addCommand } from "../lib/message";
+import { Message } from "discord.js";
 import verify from "../behaviors/verify";
+import Command from "../lib/command";
 
-addCommand("verify", (args, message) => {
-  message.mentions.members.forEach(member => verify(member));
-  return true;
-});
+export class VerifyCommand extends Command("verify") {
+  check(message: Message) {
+    return message.member.hasPermission("ADMINISTRATOR");
+  }
+
+  exec(message: Message) {
+    message.mentions.members.forEach(member => {
+      verify(member);
+    });
+    message.channel.send("Manually started verification");
+  }
+}
+
+export default new VerifyCommand();
