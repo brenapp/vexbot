af37077c16cdb6a63a48808e394f3083fcdc09b5
MayorMonty <artificalchamber@gmail.com>
2019-06-06 01:56:56 -0400

    Error reporting

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import discord from "discord.js";
 import { handleMessage } from "./lib/message";
-import "./lib/verify";
 import report from "./lib/report";
 import { client } from "./client";
 
 import "./lib/handlers";
-import "./lib/command";
-
-// import "./lib/actions/eventlog";
+import "./commands";
 
 client.on("ready", () => {
   console.log("vexbot#0599 is online!");
@@ -18,6 +15,10 @@ client.on("ready", () => {
     client.user.setActivity("over the server", { type: "WATCHING" });
   }
 });
-client.on("message", handleMessage);
 
+const reporter = report(client);
+process.on("uncaughtException", reporter);
+process.on("unhandledRejection", reporter);
+
+client.on("message", handleMessage);
 client.on("error", report);
