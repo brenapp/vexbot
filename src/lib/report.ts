a4159f197c6cac6e2ead4663fad7654a9e0d8a1c
MayorMonty <artificalchamber@gmail.com>
2019-06-11 00:41:48 -0400

    Give error enviroment

--- src/lib/report.ts
+++ src/lib/report.ts
@@ -3,6 +3,6 @@ import { Client, DiscordAPIError } from "discord.js";
 export default function report(client: Client) {
   return async (error: Error) => {
     let me = await client.fetchUser("274004148276690944");
-    me.send(error.stack);
+    me.send(`${process.env["DEV"] ? "DEV MODE" : "PRODUCTION"} ${error.stack}`);
   };
 }
