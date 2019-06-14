1526e662aaf553a8d71dd44ddcfe5ac249d712ac
MayorMonty <artificalchamber@gmail.com>
2019-06-14 03:27:43 -0400

    Allow people to edit prefixes

--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -1,6 +1,22 @@
 import { addMessageHandler } from "./message";
 
 import "./command";
+import { Command, isCommand } from "./command";
+import { client } from "../client";
 
 // Dismiss messages from a bot, we don't take their kind around here!
 addMessageHandler(message => message.author.bot);
+
+// Commands
+addMessageHandler(Command.execute);
+
+// Command editing
+client.on("messageUpdate", (old, current) => {
+  const oldIsCommand = isCommand(old);
+  const currentIsCommand = isCommand(current);
+
+  // If this just became a command, then execute it as a new command
+  if (!oldIsCommand && currentIsCommand) {
+    return Command.execute(current);
+  }
+});
