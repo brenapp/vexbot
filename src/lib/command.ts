e79d99f80dfc47c18303633fa9d122a24e88b7a4
MayorMonty <artificalchamber@gmail.com>
2019-06-14 03:07:32 -0400

    Make Command a global, exported class

--- src/lib/command.ts
+++ src/lib/command.ts
@@ -27,97 +27,104 @@ export function isCommand(message: Message) {
   return PREFIX.includes(message.content[0]);
 }
 
-export default (...names: string[]) =>
-  class Command {
-    handler: number;
-    name: string;
+export abstract class Command {
+  handler: number;
+  name: string;
 
-    constructor() {
-      this.name = names[0];
-      this.handler = addMessageHandler(this.handle);
+  constructor() {
+    this.handler = addMessageHandler(this.handle);
+  }
+
+  abstract match(message: Message): boolean;
+
+  async handle(message: Message) {
+    if (!this.match(message)) return false;
+
+    if (!(await this.check(message))) {
+      await this.fail(message);
+      return false;
     }
 
-    async handle(message: Message) {
-      if (!matchCommand(message, names)) {
-        return false;
-      }
+    // Parse args
+    const args = message.content.split(" ").slice(1);
+    const start = Date.now();
 
-      if (!(await this.check(message))) {
-        await this.fail(message);
-        return false;
-      }
+    const response = await this.exec(message, args);
 
-      // Parse args
-      const args = message.content.split(" ").slice(1);
-      const start = Date.now();
+    if (response) {
+      let message = response instanceof Array ? response[0] : response;
+      if (message.embeds.length > 0) {
+        let embed = message.embeds[0];
+        embed.footer.text += ` (took ${Date.now() - start}ms)`;
 
-      const response = await this.exec(message, args);
+        // Copy over embed
+        const replacement = makeEmbed(message)
+          .setFooter(embed.footer.text)
+          .setTitle(embed.title)
+          .setColor(embed.color)
+          .setDescription(embed.description)
+          .setImage((embed.image || { url: undefined }).url)
+          .setThumbnail((embed.thumbnail || { url: undefined }).url)
+          .setTimestamp(new Date(embed.timestamp))
+          .setURL(embed.url);
 
-      if (response) {
-        let message = response instanceof Array ? response[0] : response;
-        if (message.embeds.length > 0) {
-          let embed = message.embeds[0];
-          embed.footer.text += ` (took ${Date.now() - start}ms)`;
-
-          // Copy over embed
-          const replacement = makeEmbed(message)
-            .setFooter(embed.footer.text)
-            .setTitle(embed.title)
-            .setColor(embed.color)
-            .setDescription(embed.description)
-            .setImage((embed.image || { url: undefined }).url)
-            .setThumbnail((embed.thumbnail || { url: undefined }).url)
-            .setTimestamp(new Date(embed.timestamp))
-            .setURL(embed.url);
-
-          if (embed.author) {
-            replacement.setAuthor(embed.author);
-          }
-
-          replacement.fields = embed.fields;
-
-          message.edit({ embed: replacement });
-        } else {
-          message.edit(
-            message.content +
-              ` *(took ${Date.now() - start}ms${
-                process.env["DEV"] ? " — DEV MODE" : ""
-              })*`
-          );
+        if (embed.author) {
+          replacement.setAuthor(embed.author);
         }
+
+        replacement.fields = embed.fields;
+
+        message.edit({ embed: replacement });
+      } else {
+        message.edit(
+          message.content +
+            ` *(took ${Date.now() - start}ms${
+              process.env["DEV"] ? " — DEV MODE" : ""
+            })*`
+        );
       }
-      return true;
+    }
+    return true;
+  }
+
+  unregister() {
+    return removeMessageHandler(this.handler);
+  }
+
+  /**
+   * Check if the command can/should be run
+   * @param message
+   */
+  check(message: Message): Promise<boolean> | boolean {
+    return true;
+  }
+
+  /**
+   * Runs when check() evaluates to false
+   */
+  fail(message: Message): void | Promise<void> {
+    return;
+  }
+
+  /**
+   * Executes the command
+   * @param message
+   * @param args
+   */
+  exec(message: Message, args: string[]): Promise<Message | Message[]> | void {
+    return;
+  }
+}
+
+export default (...names: string[]) =>
+  class NamedCommand extends Command {
+    constructor() {
+      super();
+      this.name = names[0];
     }
 
-    unregister() {
-      return removeMessageHandler(this.handler);
-    }
-
-    /**
-     * Check if the command can/should be run
-     * @param message
-     */
-    check(message: Message): Promise<boolean> | boolean {
-      return true;
-    }
-
-    /**
-     * Runs when check() evaluates to false
-     */
-    fail(message: Message): void | Promise<void> {
-      return;
-    }
-
-    /**
-     * Executes the command
-     * @param message
-     * @param args
-     */
-    exec(
-      message: Message,
-      args: string[]
-    ): Promise<Message | Message[]> | void {
-      return;
+    match(message: Message) {
+      return matchCommand(message, names);
     }
   };
 
