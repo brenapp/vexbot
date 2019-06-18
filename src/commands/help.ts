f1c3be1f1bd07cf51f345ce3ebc1c390d5e70861
MayorMonty <artificalchamber@gmail.com>
2019-06-18 19:49:07 -0400

    Get all commands correctly

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,33 +1,35 @@
 import Command, { Permissions, REGISTRY, PREFIX } from "../lib/command";
 import { Message } from "discord.js";
 
-let commands = Object.values(REGISTRY).filter(
-  (cmd, i, array) => i === array.findIndex(c => c.names[0] === cmd.names[0])
-);
-
 export class HelpCommand extends Command("help") {
   check = Permissions.all;
 
   documentation() {
     return {
       description: "Lists all commands and usage",
-      usage: PREFIX + "help"
+      usage: "help"
     };
   }
 
   async exec(message: Message) {
+    let commands = Object.values(REGISTRY).filter(
+      (cmd, i, array) => i === array.findIndex(c => c.names[0] === cmd.names[0])
+    );
+
     // Get the commands that this person is able to execute
     let allowedIndex = await Promise.all(
       commands.map(cmd => cmd.check(message))
     );
     let allowed = commands.filter((cmd, i) => allowedIndex[i]);
 
+    console.log(REGISTRY);
+
     let body = "Here's what I can do!\n\n";
 
     allowed.forEach(cmd => {
       body += cmd.names.map(n => `__${n}__`).join(" or ") + ": ";
       body += cmd.documentation().description + " ";
-      body += `\`${cmd.documentation().usage}\`\n`;
+      body += `\`${PREFIX[0]}${cmd.documentation().usage}\`\n`;
     });
 
     body += "\nRemember to keep most bot usage in the appropriate channel!";
