b141b1f269d6ea638a55368c03d1c0f86a70b627
MayorMonty <artificalchamber@gmail.com>
2019-06-28 23:31:45 -0400

    Fix DOUBLE PING bug!

--- src/behaviors/log/index.ts
+++ src/behaviors/log/index.ts
@@ -1,10 +1,44 @@
 import { addMessageHandler } from "../../lib/message";
-import { TextChannel } from "discord.js";
+import { TextChannel, Guild, Message } from "discord.js";
 import { client } from "../../client";
 
 import "./important";
 
-addMessageHandler(message => {
+function matchAll(str: string, re: RegExp) {
+  return (str.match(re) || [])
+    .map(e => RegExp(re.source, re.flags).exec(e))
+    .filter(v => v !== null);
+}
+
+async function clean(message: Message) {
+  let content = message.content;
+
+  const roles = await Promise.all(
+    matchAll(content, /\<\@\&([0-9]+)\>/).map(async match => ({
+      key: match[0],
+      role: message.guild.roles.get(match[1])
+    }))
+  );
+  const members = await Promise.all(
+    matchAll(content, /\<\@\!([0-9]+)\>/).map(async match => ({
+      key: match[0],
+      member: message.guild.members.get(match[1])
+    }))
+  );
+
+  // Replace mentions
+  roles.forEach(
+    ({ key, role }) => (content = content.replace(key, `@[ROLE: ${role.name}]`))
+  );
+  members.forEach(
+    ({ key, member }) =>
+      (content = content.replace(key, `@[MEMBER: ${member.nickname}]`))
+  );
+
+  return content;
+}
+
+addMessageHandler(async message => {
   let log: TextChannel;
   if (message.channel.type === "dm") {
     return false;
@@ -22,7 +56,7 @@ addMessageHandler(message => {
   log.send(
     `${message.member.user.username}#${message.member.user.discriminator} in ${
       message.type === "dm" ? "DM" : message.channel.toString()
-    }: ${message.content.toString()}`,
+    }: ${await clean(message)}`,
     {
       files: message.attachments.map(attach => attach.url)
     }
