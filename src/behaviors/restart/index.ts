d7e86311b1cffa59b620c574bf95fd72cba29d3f
MayorMonty <artificalchamber@gmail.com>
2019-06-30 03:46:52 -0400

    Use execa instead of child_process

--- src/behaviors/restart/index.ts
+++ src/behaviors/restart/index.ts
@@ -6,8 +6,9 @@ import createHandler from "github-webhook-handler";
 import http from "http";
 import { information } from "../../lib/report";
 import { client } from "../../client";
-import child_process from "child_process";
 import { join } from "path";
+
+import execa from "execa";
 
 const handler = createHandler({ path: "/webhook", secret: "vexbot" });
 const report = information(client);
@@ -25,17 +26,11 @@ handler.on("push", function(event) {
   if (process.env["DEV"]) return;
 
   report(
-    `**PUSH RECIEVED**\n${event.payload.commits.map(
+    `**PUSH RECIEVED**\n\nCommits:${event.payload.commits.map(
       commit => `\`\`\`${commit.message}\`\`\``
     )}Deploying changes now...`
   );
-  const subprocess = child_process.spawn(
-    "sh",
-    [join(__dirname, "../../../deploy.sh")],
-    {
-      detached: true
-    }
-  );
+  const subprocess = execa.command("sh deploy.sh");
 
   subprocess.stdout.on("data", chunk => report(`\`\`\`${chunk}\`\`\``));
   subprocess.stderr.on("data", chunk => report(`\`\`\`${chunk}\`\`\``));
