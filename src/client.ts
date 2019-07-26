0c2126f8d5d29fcb049aa5bbcdff6ca9283b1c77
MayorMonty <artificalchamber@gmail.com>
2019-07-25 21:11:06 -0400

    Use access correctly

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,7 @@
 import { Client } from "discord.js";
+import { authorization } from "./lib/access";
 
-const token = process.env.token || require("../config").discord.token;
+const token = process.env.token || authorization("discord.token");
 const client = new Client();
 
 client.login(token);
