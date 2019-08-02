d9046f9aa21130128b0b1f6edb60495cb1774908
MayorMonty <artificalchamber@gmail.com>
2019-08-01 23:04:34 -0400

    Change how probations are stored

--- src/commands/probate.ts
+++ src/commands/probate.ts
@@ -3,6 +3,8 @@ import probate from "../behaviors/probation";
 import Command, { Permissions } from "../lib/command";
 import { client } from "../client";
 import { authorization } from "../lib/access";
+import SQLiteStore from "keya/out/node/sqlite";
+import keya from "keya";
 
 const owner = authorization("discord.owner");
 
@@ -12,6 +14,12 @@ export class ProbateCommand extends Command("probate", "dq") {
     message =>
       message.channel.type === "text" && !message.mentions.members.has(owner)
   );
+
+  store: SQLiteStore | null = null;
+  constructor() {
+    super();
+    keya.store("probations").then(store => (this.store = store));
+  }
 
   documentation() {
     return {
@@ -40,13 +48,44 @@ export class ProbateCommand extends Command("probate", "dq") {
     );
   }
 
-  exec(message: Message, args: string[]) {
+  async exec(message: Message, args: string[]) {
     const victims = message.mentions.members;
     const [duration, ...reason] = args.slice(victims.size);
 
     message.mentions.members.forEach(member => {
       probate(member, message.member, duration, reason.join(" "));
     });
+
+    // Update probation records
+
+    // If we don't have a reference to the database by now, then we need to wait for it.
+    if (!this.store) {
+      this.store = await keya.store("probations");
+    }
+
+    // Get records for relevant parties
+    const executor: {
+      citations: number;
+      executed: number;
+    } = (await this.store.get(message.member.id)) || {
+      citations: 0,
+      executed: 0
+    };
+    const citations: {
+      citations: number;
+      executed: number;
+    }[] = await Promise.all(
+      victims.map(victim =>
+        this.store
+          .get(victim.id)
+          .then(record => (record ? record : { citations: 0, executed: 0 }))
+      )
+    );
+
+    // Increment executor
+    executor.executed++;
+
+    // Increment victims
   }
 }
 
