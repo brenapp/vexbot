bfb3e6143ec01b58c5b44a7e8d406beb1e04aee2
MayorMonty <artificalchamber@gmail.com>
2019-08-01 23:06:09 -0400

    Add back the copypasta

--- src/behaviors/random.ts
+++ src/behaviors/random.ts
@@ -16,6 +16,19 @@ addMessageHandler(message => {
   message.react(ping);
 });
 
+// Navy Seal Copypasta
+addMessageHandler(message => {
+  if (
+    !message.mentions.users.has(client.user.id) &&
+    !message.content.toLowerCase().includes("fuck you")
+  )
+    return false;
+
+  message.reply(
+    "What the fuck did you just fucking say about me, you little human? I'll have you know I graduated top of my class in the Bot Academy, and I've been involved in numerous secret raids on discord servers, and I have over 300 confirmed bans. I am trained in robot warfare and I'm the top moderator in the entire US armed forces. You are nothing to me but just another user. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words.  You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of Bradleys across Discord and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your account. You're fucking dead, kid. I can be anywhere, anytime, and I can ban you in over seven hundred ways, and that's just automatically. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the Vex Teams of South Carolina Discord and I will use it to its full extent to wipe your miserable ass off the face of the server, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo."
+  );
+});
+
 // Rename #regret to something random
 addMessageHandler(async message => {
   if (!isCommand(message)) return false;
@@ -53,13 +66,4 @@ addMessageHandler(async message => {
   channel.setName(name);
 
   return false;
-});
-
-addMessageHandler(message => {
-  if (!DEBUG || message.channel.id !== "463891502473543690") {
-    return false;
-  }
-
-  message.channel.send(code(message.content));
-  return true;
 });
