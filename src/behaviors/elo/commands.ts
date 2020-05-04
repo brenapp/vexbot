3109deea45694cf599dc35a20f85faec1b70bcd3
Brendan McGuire <brendan@bren.app>
2020-05-04 05:10:09 -0400

    use updated desc ssyntax

--- src/behaviors/elo/commands.ts
+++ src/behaviors/elo/commands.ts
@@ -5,14 +5,12 @@ import { updateElo } from ".";
 export class UpdateEloCommand extends Command("eloupdate") {
   check = Permissions.owner;
 
-  documentation() {
-    return {
-      description:
-        "Updates ELO Rankings for a given season (only call if something is wrong)",
-      usage: "eloupdate Tower Takeover",
-      group: "VEX",
-    };
-  }
+  documentation = {
+    description:
+      "Updates ELO Rankings for a given season (only call if something is wrong)",
+    usage: "eloupdate Tower Takeover",
+    group: "VEX",
+  };
 
   async exec(message: Message, args: string[]) {
     const season = args.join(" ");
