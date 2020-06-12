b71050bbe6000aa03f802c81c9800d9d1f88b05b
Brendan McGuire <brendan@bren.app>
2020-06-12 01:36:46 -0400

    sweepMessages from cache

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,14 @@
 import { Client } from "discord.js";
-import { authorization } from "./lib/access";
+import { authorization, config } from "./lib/access";
 
 const token = process.env.token || (authorization("discord.token") as string);
 const client = new Client();
 
 client.login(token);
+
+setInterval(() => {
+  const cleaned = client.sweepMessages();
+  console.log(`Cleared ${cleaned} messages from cache`);
+}, config("memory.cleanInterval") as number);
 
 export { client };
