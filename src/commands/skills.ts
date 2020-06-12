94ed4f33e394fb48bdb3c97e3532e7c349a49b2e
Brendan McGuire <brendan@bren.app>
2020-06-12 18:31:02 -0400

    Support regions and vexu

--- src/commands/skills.ts
+++ src/commands/skills.ts
@@ -15,6 +15,16 @@ import {
   SkillsResponseObject,
 } from "vexdb/out/constants/ResponseObjects";
 
+async function getSkillsAtEvents(events: string[]) {
+  const runs: SkillsResponseObject[] = [];
+
+  for (const sku of events) {
+    runs.push(...(await vexdb.get("skills", { sku })));
+  }
+
+  return runs;
+}
+
 function findBest(type: number, skills: SkillsResponseObject[]) {
   let best: SkillsResponseObject | null = null;
 
@@ -95,6 +105,52 @@ async function teamRecord(
   return embed;
 }
 
+async function groupRecord(
+  skillsData: SkillsResponseObject[],
+  label: string,
+  message: Message
+): Promise<MessageEmbed> {
+  const embed = makeEmbed(message);
+
+  const skills: {
+    [team: string]: (SkillsResponseObject | null)[];
+  } = {};
+
+  for (const run of skillsData) {
+    // Create the skills object if it doesn't exist
+    if (!skills[run.team]) {
+      skills[run.team] = [null, null, null];
+    }
+
+    // Update it if it's greater
+    if (
+      !skills[run.team][run.type] ||
+      (skills[run.team][run.type] as SkillsResponseObject).score < run.score
+    ) {
+      skills[run.team][run.type] = run;
+      continue;
+    }
+  }
+
+  // Get the top teams
+  const top = Object.keys(skills)
+    .filter((team) => skills[team][2]) // Only consider teams who have Robot Skills Score
+    .sort((a, b) => (skills[b][2]?.score || 0) - (skills[a][2]?.score || 0)); // Sort by highest score
+
+  let body = "";
+
+  for (const [index, team] of Object.entries(top.slice(0, 10))) {
+    const [driver, programming, robot] = skills[team];
+
+    body += `${+index + 1}. ${team} — ${robot?.score} (${driver?.score ||
+      0} + ${programming?.score || 0})\n`;
+  }
+
+  embed.setAuthor(`${label} Skills Leaderboard`).setDescription(body);
+
+  return embed;
+}
+
 export const SkillsCommand = Command({
   names: ["skills"],
 
@@ -106,14 +162,60 @@ export const SkillsCommand = Command({
 
   check: Permissions.all,
 
-  async exec(message: Message, [teamOrRegion]: string[]) {
+  async exec(message: Message, args: string[]) {
+    const teamOrRegion = args.join(" ");
+
+    let embed: MessageEmbed;
+
     // First see if there is a team
     const team = await vexdb.get("teams", { team: teamOrRegion });
 
-    if (team.length > 0) {
-      const embed = await teamRecord(team[0], message);
+    let intermediate: Message | undefined = undefined;
 
-      return message.channel.send({ embed });
+    if (team.length > 0) {
+      intermediate = await message.channel.send(
+        `Getting skills data for ${teamOrRegion}...`,
+        { split: false }
+      );
+
+      embed = await teamRecord(team[0], message);
+    } else if (teamOrRegion.toUpperCase() === "VEXU") {
+      intermediate = await message.channel.send(
+        `Getting skills data for ${teamOrRegion}...`,
+        { split: false }
+      );
+
+      const events = await vexdb.get("events", {
+        program: "VEXU",
+        season: "Tower Takeover",
+      });
+      const skillsData = await getSkillsAtEvents(events.map((r) => r.sku));
+
+      embed = await groupRecord(skillsData, "VEXU", message);
+    } else {
+      intermediate = await message.channel.send(
+        `Getting skills data for ${teamOrRegion}...`,
+        { split: false }
+      );
+
+      const events = await vexdb.get("events", {
+        region: teamOrRegion,
+        season: "Tower Takeover",
+      });
+
+      if (events.length < 1) {
+        return message.channel.send(
+          "No team or region `" + teamOrRegion + "`."
+        );
+      }
+
+      const skillsData = await getSkillsAtEvents(events.map((r) => r.sku));
+
+      embed = await groupRecord(skillsData, teamOrRegion, message);
     }
+
+    intermediate.delete();
+
+    return message.channel.send({ embed });
   },
 });
