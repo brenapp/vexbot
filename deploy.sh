12ca82c12319a3ab7408c407adecca771afbc6b6
Brendan McGuire <brendan@bren.app>
2020-07-06 21:06:59 -0400

    Ignore strict host checking and build correct tarball

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 # In Continous Integration, deploys the product to the server
-rsync vexbot.tgz ec2-user@dev.bren.app:~/vexbot
+rsync -o "StrictHostKeyChecking no" vexbot.tgz ec2-user@dev.bren.app:~/vexbot
 
 ssh ec2-user@dev.bren.app tar -xvf ~/vexbot.tgz
