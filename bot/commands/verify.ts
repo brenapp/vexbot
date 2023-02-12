a80f589ee06f851096ac5d213b8f3600948fa35d
Brendan McGuire <brendan@bren.app>
2023-02-11 21:13:19 -0500

    Verification System

--- bot/commands/verify.ts
+++ bot/commands/verify.ts
@@ -1,5 +1,12 @@
-import { TextChannel } from "discord.js";
-import { client } from "main";
+import { ActionRowBuilder, EmbedBuilder } from "@discordjs/builders";
+import {
+  ButtonBuilder,
+  ButtonStyle,
+  CreateRoleOptions,
+  MessageComponentInteraction,
+  TextChannel,
+} from "discord.js";
+import * as robotevents from "robotevents";
 import Command, { Permissions } from "~lib/command";
 
 const VerifyCommand = Command({
@@ -31,7 +38,7 @@ const VerifyCommand = Command({
             .setChoices(
               { name: "Member", value: "MEMBER" },
               { name: "Alumnus", value: "ALUM" },
-              { name: "Mentor", value: "Mentor" }
+              { name: "Mentor", value: "MENTOR" }
             )
             .setRequired(true)
         )
@@ -49,21 +56,120 @@ const VerifyCommand = Command({
             .setRequired(false)
         ),
   },
-  check: Permissions.admin,
+  check: Permissions.always,
 
   async exec(interaction) {
     const user = interaction.user;
-    const name = interaction.options.getString("name", true);
-    const team = interaction.options.getString("team", true);
-    const role = interaction.options.getString("role", true);
-    const pronouns = interaction.options.getString("pronouns", true);
+    const name = interaction.options.get("name", true).value as string;
+    const number = interaction.options.get("team", true).value as string;
+    const role = interaction.options.get("role", true).value as
+      | "MEMBER"
+      | "ALUM"
+      | "MENTOR";
+    const pronouns = interaction.options.get("pronouns", false)?.value as
+      | string
+      | undefined;
 
-    console.log({
-      user,
-      name,
-      team,
-      role,
-      pronouns,
+    async function findOrCreateRole(name: string, options?: CreateRoleOptions) {
+      const role = interaction.guild!.roles.cache.find(
+        (role) => role.name === name
+      );
+
+      if (role) {
+        return role;
+      }
+
+      return await interaction.guild!.roles.create({
+        name,
+        ...options,
+      });
+    }
+
+    const team = await robotevents.teams.get(number);
+
+    if (!team) {
+      await interaction.reply({
+        content: `Sorry, I couldn't find a team with the number \`${number}.\``,
+        ephemeral: true,
+      });
+      return;
+    }
+
+    const localTeam = team.location.region === "South Carolina";
+    const roleNotation = {
+      MEMBER: "",
+      ALUM: "Alum",
+      MENTOR: "Mentor",
+    };
+    const nickname = `${name} | ${team.number} ${roleNotation[role]}`.trim();
+    const member = await interaction.guild!.members.fetch(user.id);
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Verification for ${nickname}`)
+      .setDescription(
+        `Welcome to the server, ${name}! A moderator will verify your information shortly. This verification request will expire in 24 hours.`
+      )
+      .addFields(
+        { name: "Team", value: number },
+        { name: "Position", value: role },
+        { name: "Pronouns", value: pronouns ?? "Not Specified" }
+      );
+
+    const teamRole = localTeam
+      ? await findOrCreateRole(team.location.region)
+      : await findOrCreateRole(team.location.region, { mentionable: true });
+    const gradeRole = await findOrCreateRole(team.grade);
+    const verified = await findOrCreateRole("Verified");
+    const pronounRole = pronouns ? await findOrCreateRole(pronouns) : undefined;
+
+    const approve = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId("verify-approve")
+        .setLabel("Moderator: Approve")
+        .setStyle(ButtonStyle.Success)
+    );
+
+    const filter = (i: MessageComponentInteraction) =>
+      i.customId === "verify-approve" &&
+      (i.memberPermissions?.has("ManageRoles") ?? false);
+
+    const collector = interaction.channel!.createMessageComponentCollector({
+      filter,
+      time: 1000 * 60 * 60 * 24,
+    });
+
+    collector.on("collect", async (i) => {
+      await i.deferUpdate();
+
+      try {
+        await member.setNickname(nickname);
+        await member.roles.add([verified, teamRole, gradeRole]);
+        if (pronounRole) await member.roles.add(pronounRole);
+        await i.editReply({
+          content: `Successfully verified ${member}!`,
+          components: [],
+        });
+      } catch (e) {
+        console.log(e);
+        await i.editReply({
+          content: `Failed to verify ${member}:\n \`\`\`${e}\`\`\``,
+          components: [],
+        });
+      }
+    });
+
+    collector.on("end", async (collected) => {
+      if (collected.size === 0) {
+        await interaction.editReply({
+          content: `Verification for ${member} has expired.`,
+          components: [],
+        });
+      }
+    });
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [approve],
     });
   },
 });
